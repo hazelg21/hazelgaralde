7bfe207f67b0cf7789071bcb0c1b509ae9d76f12
Hazel Garalde <hlgaralde21@gmail.com>
2022-04-05 23:22:40 +0800

    update project description

--- src/Pages/portfolio.js
+++ src/Pages/portfolio.js
@@ -8,21 +8,21 @@ export default function PortfolioPage() {
         name: 'My Portfolio',
         f_end: "ReactJS",
         b_end: "NodeJS (For Sending Email)",
-        desc: "This website! I have created this website as my portfolio. The design also allows mobile responsiveness!",
+        desc: "This website! I have created this website as my portfolio. The design also allows mobile responsiveness.",
         link: "(This website)",
         logo: "myportfolio.JPG"
     }, {
         name: 'Hermosa',
         f_end: "ReactJS",
         b_end: "NodeJS, MongoDB",
-        desc: "Hermosa is an Ecommerce website for selling hair products. This website was created using React, HTML, CSS and Javascript for front-end. For back-end, I used MongoDB, NodeJS and ExpressJS. I am still in the process of adding additional features, updating the design and mobile responsiveness.",
+        desc: "Hermosa is an e-commerce website for selling hair products. This website was created using React, HTML, CSS, and Javascript for the front-end. For the back-end, I used MongoDB, NodeJS, and ExpressJS. I am still in the process of adding additional features and updating the design and mobile responsiveness.",
         link: "https://capstone3-vercel.vercel.app",
         logo: "hermosa.JPG"
     }, {
         name: 'Time Tracker App',
         f_end: "ReactJS",
         b_end: "NodeJS, MongoDB",
-        desc: "Time Tracker App is one of my ongoing projects. When working for a project it is important to monitor the steps taken to fix issues and create features. The time tracker app will help determine which tag and activity took the most time todo.",
+        desc: "Time Tracker App is one of my ongoing projects. When working on a project it is important to monitor the steps taken to fix issues and create features. The time tracker app will help determine which tag and activity took the most time to do.",
         link: "https://time-tracker-app-alpha.vercel.app",
         logo: "timetracker.JPG"
     },
